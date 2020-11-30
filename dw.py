6d449674c8a813c0b673721ecef930fa00460d24
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-29 21:16:41 -0600

    added tkinter version

--- dw.py
+++ dw.py
@@ -1,17 +1,47 @@
 from pytube import YouTube
+from tkinter import *
 
 '''
 Documentation: https://pypi.org/project/pytube/
 '''
 
-SAVE_PATH = "E:/"
+# SAVE_PATH = "E:/"
 
-link = "https://www.youtube.com/watch?v=HHBsvKnCkwI"
+# link = "https://www.youtube.com/watch?v=HHBsvKnCkwI"
 
-try:
-    youtube = YouTube(link)
-except:
-    print("Connection error")
+# try:
+#     youtube = YouTube(link)
+# except:
+#     print("Connection error")
 
-stream = youtube.streams.first()
-stream.download()
+# stream = youtube.streams.first()
+# stream.download()
+
+root = Tk()
+
+root.geometry("500x350")
+
+root.title("Download YouTube Videos.")
+
+def dw():
+    '''
+    Download youtube videos without errors
+    '''
+    try:
+        myVar.set("DOWNLOADING...")
+        root.update()
+        YouTube(link.get()).streams.first().download()
+        link.set("Download successful.")
+    except:
+        myVar.set("Error found...")
+        root.updated()
+        link.set("Enter correct link")
+
+Label(root, text="Welcome to Youtube\nDownloader")
+myVar = StringVar()
+myVar.set("Enter a url below:")
+Entry(root, textvariable=myVar, width=40).pack(pady=10)
+link = StringVar()
+Entry(root, textvariable=link, width=40).pack(pady=10)
+Button(root, text="Download", command=dw).pack()
+root.mainloop()
